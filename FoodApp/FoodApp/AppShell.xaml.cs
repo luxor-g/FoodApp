e27997706244ae01369b714a1172df0cacda88f5
luxor <miguelangelpuertassanchez@gmail.com>
2022-05-10 17:38:06 +0100

    Add project files.

---
+++ FoodApp/FoodApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FoodApp.ViewModels;
+using FoodApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FoodApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ FoodApp/FoodApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FoodApp.Models;
+using FoodApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
